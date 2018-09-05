94fc02f3b84a3f9fc8432632045c56ebbd37070e
rafaesc <rafaesc92@gmail.com>
2018-09-05 11:25:40 -0500

    fix: Update config karma

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,6 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
     browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
@@ -35,6 +34,6 @@ module.exports = function (config) {
         flags: ['--no-sandbox']
       }
     },
-    singleRun: true
+    singleRun: false
   });
 };
